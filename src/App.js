74f7a647560b91fa96de20f67694662e6a124b22
LR <lr177245@gmail.com>
2020-08-13 11:17:44 -0700

    display data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import routes from './data.js';
 import './App.css';
 
+class DataContainer extends Component {
+  state = {
+    data: routes,
+  }
+
+  render() {
+    const data = this.state.data.routes.map(elem => {
+      return <Data stuff={elem} />
+    });
+
+    return (
+      <dl>
+        {data}
+      </dl>
+    );
+  }
+}
+
+class Data extends Component {
+  render() {
+    return (
+      <dd>
+        <span>{this.props.stuff.airline}</span>
+        <span>{this.props.stuff.src}</span>
+        <span>{this.props.stuff.dest}</span>
+      </dd>
+    );
+  }
+}
+
 class App extends Component {
+
   render() {
     return (
       <div className="app">
@@ -9,9 +41,7 @@ class App extends Component {
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <DataContainer />
         </section>
       </div>
     );
